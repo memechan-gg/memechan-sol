b2843c37a74977833fec0f90c4a6a47c988482ba
0xxgen <160734182+0xxgen@users.noreply.github.com>
2024-05-12 09:25:11 +0100

    Update localent deployment

--- bin/deploy_local.sh
+++ bin/deploy_local.sh
@@ -1,7 +1,6 @@
 solana config set --url localhost
 
 solana airdrop 1000
-solana airdrop 1000 CXkuDmMcnCAme9aMHzDqhnyuXvD7q6UUBFMupuVxLPZt
 
 solana program deploy tests/programs/openbook.so  --keypair keypairs/kp.json # openbook: 6jXACiuWwGjc2Hq7rzz7mLGZZnAjAgS6noCYtB31xx4u
 solana program deploy tests/programs/raydium.so  --keypair keypairs/kp.json # raydium: EA68RLWrCRtwbiudgb25mHrFzuLBfCEVVhHEeuFF9sC6
