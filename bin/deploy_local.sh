3326f4852b4b8bbccea2ce853eced6a94d863c77
0xxgen <160734182+0xxgen@users.noreply.github.com>
2024-05-12 09:23:53 +0100

    Update localent deployment

--- bin/deploy_local.sh
+++ bin/deploy_local.sh
@@ -6,4 +6,4 @@ solana airdrop 1000 CXkuDmMcnCAme9aMHzDqhnyuXvD7q6UUBFMupuVxLPZt
 solana program deploy tests/programs/openbook.so  --keypair keypairs/kp.json # openbook: 6jXACiuWwGjc2Hq7rzz7mLGZZnAjAgS6noCYtB31xx4u
 solana program deploy tests/programs/raydium.so  --keypair keypairs/kp.json # raydium: EA68RLWrCRtwbiudgb25mHrFzuLBfCEVVhHEeuFF9sC6
 
-anchor deploy
+anchor deploy  --program-name memechan_sol --program-keypair keypairs/kp.json
