87639fac9e3b74dd2cd20cb709904b402dd3ee00
0xxgen <160734182+0xxgen@users.noreply.github.com>
2024-05-13 10:36:20 +0100

    Fix local deployment script

--- bin/deploy_local.sh
+++ bin/deploy_local.sh
@@ -2,7 +2,7 @@ solana config set --url localhost
 
 solana airdrop 1000
 
-solana program deploy tests/programs/openbook.so  --keypair keypairs/kp.json # openbook: 5LwV4JK4ExCPJGJqhJLEQcHPeVsUX1omRo97jH6BvymW
-solana program deploy tests/programs/raydium.so  --keypair keypairs/kp.json # raydium: FszPLAESehPmaW69NAGBLzRLFNFNjPQXNCfSEac8nst
+solana program deploy tests/programs/openbook.so --program-id keypairs/openbook.json # openbook: 5LwV4JK4ExCPJGJqhJLEQcHPeVsUX1omRo97jH6BvymW
+solana program deploy tests/programs/raydium.so --program-id keypairs/raydium.json # raydium: FszPLAESehPmaW69NAGBLzRLFNFNjPQXNCfSEac8nst
 
 anchor deploy  --program-name memechan_sol --program-keypair keypairs/kp.json
