c3cfe1fc0dd2d217f48cfcb31a242af76b45b706
Igor Koval <comradekoval@gmail.com>
2024-05-31 07:11:10 +0800

    feat: new program address and consts

--- programs/memechan-sol/src/consts.rs
+++ programs/memechan-sol/src/consts.rs
@@ -49,11 +49,16 @@ pub const SLERF_MINT: Pubkey =
 
 #[cfg(feature = "mainnet")]
 pub const SLERF_MINT: Pubkey =
-    solana_program::pubkey!("7BgBvyjrZX1YKz4oh9mjb8ZScatkkwb8DzFx7LoiVkM3"); // Mainnet
+    solana_program::pubkey!("9pECN2xxLQo22bFYpsNr3T3eW1UdEDtSqPQopFrGv7n4"); // Mainnet
 
 #[cfg(not(feature = "mainnet"))]
 pub const ADMIN_KEY: Pubkey =
     solana_program::pubkey!("8SvkUtJZCyJwSQGkiszwcRcPv7c8pPSr8GVEppGNN7DV");
 #[cfg(feature = "mainnet")]
 pub const ADMIN_KEY: Pubkey =
-    solana_program::pubkey!("KZbAoMgCcb2gDEn2Ucea86ux84y25y3ybbWQGQpd9D6");
+    solana_program::pubkey!("9pECN2xxLQo22bFYpsNr3T3eW1UdEDtSqPQopFrGv7n4");
+
+#[cfg(not(feature = "mainnet"))]
+pub const FEE_KEY: Pubkey = solana_program::pubkey!("feeLPZEfzJFwDR11cdMWE3nSa4nr7sPPM4u6tmDTw3Y");
+#[cfg(feature = "mainnet")]
+pub const FEE_KEY: Pubkey = solana_program::pubkey!("feeLPZEfzJFwDR11cdMWE3nSa4nr7sPPM4u6tmDTw3Y");
