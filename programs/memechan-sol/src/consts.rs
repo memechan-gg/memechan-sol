c63c05050ee19af00d9ba3e9a5b36e1b88c8d952
Igor Koval <comradekoval@gmail.com>
2024-06-20 09:40:36 +0800

    fix: removed unused raydium constants

--- programs/memechan-sol/src/consts.rs
+++ programs/memechan-sol/src/consts.rs
@@ -28,24 +28,6 @@ pub const DEFAULT_LINEAR: i64 = 1800; // 0.5 hours;
 #[cfg(feature = "mainnet")]
 pub const DEFAULT_LINEAR: i64 = 604_800; // 7 days;
 
-// Raydium seeds
-/// Suffix for amm authority seed
-pub const AUTHORITY_AMM: &'static [u8] = b"amm authority";
-/// Suffix for amm associated seed
-pub const AMM_ASSOCIATED_SEED: &'static [u8] = b"amm_associated_seed";
-/// Suffix for target associated seed
-pub const TARGET_ASSOCIATED_SEED: &'static [u8] = b"target_associated_seed";
-/// Suffix for amm open order associated seed
-pub const OPEN_ORDER_ASSOCIATED_SEED: &'static [u8] = b"open_order_associated_seed";
-/// Suffix for coin vault associated seed
-pub const COIN_VAULT_ASSOCIATED_SEED: &'static [u8] = b"coin_vault_associated_seed";
-/// Suffix for pc vault associated seed
-pub const PC_VAULT_ASSOCIATED_SEED: &'static [u8] = b"pc_vault_associated_seed";
-/// Suffix for lp mint associated seed
-pub const LP_MINT_ASSOCIATED_SEED: &'static [u8] = b"lp_mint_associated_seed";
-/// Amm config seed
-pub const AMM_CONFIG_SEED: &'static [u8] = b"amm_config_account_seed";
-
 #[cfg(not(feature = "mainnet"))]
 pub const SLERF_MINT: Pubkey =
     solana_program::pubkey!("HX2pp5za2aBkrA5X5iTioZXcrpWb2q9DiaeWPW3qKMaw"); // Devnet - can use any
